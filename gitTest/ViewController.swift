e5cd6c80d6552c4ddda149060a8117886cbd6420
Michael Lipshin <lipshin@Michaels-MBP.Dlink>
2021-08-03 09:12:24 +0300

    first commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Michael Lipshin on 03/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("hello world")
+    }
+
+
+}
+
